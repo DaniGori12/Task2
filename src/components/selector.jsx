782fb470f7ef91500cfc8bdbb1107d19558123cc
Dani <dani.gregori@imaginagroup.com>
2023-10-06 12:57:22 +0200

    asdfg

--- src/components/selector.jsx
+++ src/components/selector.jsx
@@ -3,37 +3,45 @@ import { useState } from "react"
 
 export default function Selector() {
 
-    const [selectedData, setSelectedData] = useState();
+    const [selectedData, setSelectedData] = useState(false);
+
 
     function dataSelect(event) {
+        console.log("estás seleccionando una opción")
         setSelectedData(Database[event.target.value])
     }
 
     return (
         <div >
-  
-            <select onChange={dataSelect}>
+
+            <select onChange={dataSelect} defaultValue="default">
+                <option disabled value="default" hidden>Choose an option</option>
                 {Database.map((element, id) => {
                     return (
-                        <option value={id} key={id}>
+                        <>
+                            <option value={id} key={id}>
 
-                            <>{element.name}</>
- 
-                        </option>
+                                {element.name}
+
+                            </option>
+                        </>
                     )
                 })}
             </select>
-  <div className="select">
-            { selectedData ? 
-                <>
-<div className="idSelector">El ID del curso es: {selectedData.id}</div>
-                    <h1>{selectedData.name}</h1>
-                    <h2>{selectedData?.description}</h2>
-                    <img className="image" src={selectedData.img} />
-                </> 
-            : <h1>Selecciona un curso</h1>
-            }
-  </div>
+            <div className="select">
+                {selectedData ?
+                    <>
+                        <div className="idSelector">El ID del curso es: {selectedData.id}</div>
+                        <h1>{selectedData.name}</h1>
+                        <h2>{selectedData?.description}</h2>
+                        <img className="image" src={selectedData.img} />
+                    </>
+                    :
+                    <div className="ternario">
+                        <h1>Selecciona un curso</h1>
+                    </div>
+                }
+            </div>
         </div>
     )
 }
